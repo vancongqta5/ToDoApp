41a67082d9487a2a83008c8225074f93e8726f62
vancongqta5 <vancongqta5@gmail.com>
2023-03-24 10:11:24 +0700

    Revert "fix jwt 3"
    This reverts commit f437c9d59a328fd87c8eab1d65ff5deb774ddfdb.

--- src/main/java/com/example/todoapp/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/todoapp/security/JwtAuthenticationFilter.java
@@ -1,37 +1,37 @@
-package com.example.todoapp.security;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.util.StringUtils;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
-
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    @Autowired
-    private JwtTokenUtil jwtTokenUtil;
-
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-        String token = jwtTokenUtil.getTokenFromRequest(request);
-
-        if (StringUtils.hasText(token) && jwtTokenUtil.validateToken(token, userDetailsService.loadUserByUsername(jwtTokenUtil.getUsernameFromToken(token)))) {
-            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                    jwtTokenUtil.getUsernameFromToken(token), null, new ArrayList<>());
-            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        }
-        filterChain.doFilter(request, response);
-    }
-}
+//package com.example.todoapp.security;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+//import org.springframework.util.StringUtils;
+//import org.springframework.web.filter.OncePerRequestFilter;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.util.ArrayList;
+//
+//public class JwtAuthenticationFilter extends OncePerRequestFilter {
+//    @Autowired
+//    private JwtTokenUtil jwtTokenUtil;
+//
+//    @Autowired
+//    private UserDetailsService userDetailsService;
+//
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+//            throws ServletException, IOException {
+//        String token = jwtTokenUtil.getTokenFromRequest(request);
+//
+//        if (StringUtils.hasText(token) && jwtTokenUtil.validateToken(token, userDetailsService.loadUserByUsername(jwtTokenUtil.getUsernameFromToken(token)))) {
+//            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+//                    jwtTokenUtil.getUsernameFromToken(token), null, new ArrayList<>());
+//            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+//            SecurityContextHolder.getContext().setAuthentication(authentication);
+//        }
+//        filterChain.doFilter(request, response);
+//    }
+//}
--- src/main/java/com/example/todoapp/service/UserServiceImpl.java
+++ src/main/java/com/example/todoapp/service/UserServiceImpl.java
@@ -1,50 +1,50 @@
-    package com.example.todoapp.service;
+package com.example.todoapp.service;
 
-    import com.example.todoapp.dto.RegistrationRequest;
-    import com.example.todoapp.models.User;
-    import com.example.todoapp.repository.UserRepository;
-    import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.security.crypto.password.PasswordEncoder;
-    import org.springframework.stereotype.Service;
+import com.example.todoapp.dto.RegistrationRequest;
+import com.example.todoapp.models.User;
+import com.example.todoapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
-    @Service
-    public class UserServiceImpl implements UserService {
-        @Autowired
-        private UserRepository userRepository;
-        @Autowired
-        private PasswordEncoder passwordEncoder;
-        @Override
-        public User registerUser(RegistrationRequest registrationRequest) throws Exception {
-            // kiểm tra xem tên đăng nhập hoặc email đã được sử dụng hay chưa
-            if (userRepository.existsByUsername(registrationRequest.getUsername())) {
-                throw new Exception("Tên đăng nhập đã được sử dụng");
-            }
-            if (userRepository.existsByEmail(registrationRequest.getEmail())) {
-                throw new Exception("Email đã được sử dụng");
-            }
-
-            // tạo đối tượng User mới từ RegistrationRequest
-            User user = new User();
-            user.setUsername(registrationRequest.getUsername());
-            user.setEmail(registrationRequest.getEmail());
-            user.setPassword(passwordEncoder.encode(registrationRequest.getPassword()));
-
-            // lưu User mới vào cơ sở dữ liệu
-            return userRepository.save(user);
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Override
+    public User registerUser(RegistrationRequest registrationRequest) throws Exception {
+        // kiểm tra xem tên đăng nhập hoặc email đã được sử dụng hay chưa
+        if (userRepository.existsByUsername(registrationRequest.getUsername())) {
+            throw new Exception("Tên đăng nhập đã được sử dụng");
+        }
+        if (userRepository.existsByEmail(registrationRequest.getEmail())) {
+            throw new Exception("Email đã được sử dụng");
         }
 
-        @Override
-        public boolean existsByUsername(String username) {
-            return userRepository.existsByUsername(username);
-        }
+        // tạo đối tượng User mới từ RegistrationRequest
+        User user = new User();
+        user.setUsername(registrationRequest.getUsername());
+        user.setEmail(registrationRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registrationRequest.getPassword()));
 
-        @Override
-        public boolean existsByEmail(String email) {
-            return userRepository.existsByEmail(email);
-        }
-
-        @Override
-        public void save(User user) {
-
-        }
+        // lưu User mới vào cơ sở dữ liệu
+        return userRepository.save(user);
     }
+
+    @Override
+    public boolean existsByUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    @Override
+    public void save(User user) {
+
+    }
+}
