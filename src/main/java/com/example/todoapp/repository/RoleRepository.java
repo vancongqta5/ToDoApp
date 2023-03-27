a75eca11740a35a4285da4126d973b28470365ab
vancongqta5 <vancongqta5@gmail.com>
2023-03-28 01:22:24 +0700

    User And Role Repository

---
+++ src/main/java/com/example/todoapp/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.todoapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import javax.management.relation.Role;
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+}
---
+++ src/main/java/com/example/todoapp/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.todoapp.repository;
+
+import com.example.todoapp.models.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByUsername(String username);
+    Boolean existsByUsername(String username);
+}
