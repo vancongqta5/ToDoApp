18aeb6f29521447ebcdd290e23d244ea5b5b271f
vancongqta5 <vancongqta5@gmail.com>
2023-03-28 01:34:35 +0700

    Login

--- src/main/java/com/example/todoapp/controllers/AuthController.java
+++ src/main/java/com/example/todoapp/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package com.example.todoapp.controllers;
 
+import com.example.todoapp.dto.LoginDto;
 import com.example.todoapp.dto.RegisterDto;
 import com.example.todoapp.models.Role;
 import com.example.todoapp.models.UserEntity;
@@ -9,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,6 +39,15 @@ public class AuthController {
         this.passwordEncoder = passwordEncoder;
     }
 
+    @PostMapping("login")
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDto.getUsername(),
+                        loginDto.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return new ResponseEntity<>("User signed success!", HttpStatus.OK);
+    }
     @PostMapping("register")
     public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
         if (userRepository.existsByUsername(registerDto.getUsername())) {
