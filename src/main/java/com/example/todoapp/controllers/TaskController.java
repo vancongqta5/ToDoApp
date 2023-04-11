8e3802c2fad6a4596352a6ce5f7d38524925a4ec
vancongqta5 <vancongqta5@gmail.com>
2023-04-11 13:00:57 +0700

    edit CRUD done

--- src/main/java/com/example/todoapp/controllers/TaskController.java
+++ src/main/java/com/example/todoapp/controllers/TaskController.java
@@ -1,28 +1,38 @@
 package com.example.todoapp.controllers;
 
 import com.example.todoapp.models.Task;
+import com.example.todoapp.repository.TaskRepository;
 import com.example.todoapp.service.TaskService;
+import com.example.todoapp.exception.ResourceNotFoundException;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import java.beans.PropertyDescriptor;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
     @Autowired
     private TaskService taskService;
+    @Autowired
+    private TaskRepository taskRepository;
 
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Successfully retrieved list"),
             @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
             @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
-            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+            @ApiResponse(code = 404, message = "Task not found with the given ID"),
             @ApiResponse(code = 500, message = "Internal server error")})
 
     @GetMapping("")
@@ -30,14 +40,24 @@ public class TaskController {
         return taskService.getAllTasks();
     }
 
-    @GetMapping("name/{name}")
-    public List<Task> getTasksByName(@PathVariable String name) {
-        return taskService.getTaskByName(name);
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        Task task = taskService.getTaskById(id);
+        if (task == null) {
+            throw new ResourceNotFoundException("Task not found with id " + id);
+        }
+        return ResponseEntity.ok(task);
     }
-    @GetMapping("id/{id}")
-    public Task getTaskById(@PathVariable Long id) {
-        return taskService.getTaskById(id);
+
+    @GetMapping("/name/{name}")
+    public ResponseEntity<List<Task>> getTasksByName(@PathVariable String name) {
+        List<Task> tasks = taskService.getTaskByName(name);
+        if (tasks.isEmpty()) {
+            throw new ResourceNotFoundException("Task not found with name " + name);
+        }
+        return ResponseEntity.ok(tasks);
     }
+
 
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
@@ -47,15 +67,43 @@ public class TaskController {
         return taskService.saveTask(task);
     }
 
-    @PutMapping("/{id}")
-    public Task updateTask(@PathVariable("id") Long id, @RequestBody Task task) {
+    @PatchMapping("/patch/{id}")
+    public ResponseEntity<Task> updateTaskByPatch(@PathVariable("id") Long id, @RequestBody Task partialUpdate) {
         Task existingTask = taskService.getTaskById(id);
-        BeanUtils.copyProperties(task, existingTask, "id");
+        if (existingTask == null) {
+            throw new ResourceNotFoundException("Task not found with id " + id);
+        }
+        BeanUtils.copyProperties(partialUpdate, existingTask, getNullPropertyNames(partialUpdate));
+        Task updatedTask = taskService.saveTask(existingTask);
+        return ResponseEntity.ok(updatedTask);
+    }
+
+    private static String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        return Stream.of(src.getPropertyDescriptors())
+                .map(PropertyDescriptor::getName)
+                .filter(name -> src.getPropertyValue(name) == null)
+                .toArray(String[]::new);
+    }
+
+    @PutMapping("/put/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
+        Task existingTask = taskService.getTaskById(id);
+        if (existingTask == null) {
+            throw new ResourceNotFoundException("Task not found with id " + id);
+        }
+        BeanUtils.copyProperties(updatedTask, existingTask, "id");
         return taskService.saveTask(existingTask);
     }
 
-    @DeleteMapping("/{id}")
-    public void deleteTask(@PathVariable Long id) {
-        taskService.deleteTaskById(id);
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Object> deleteTaskById(@PathVariable Long id) {
+        Optional<Task> optionalTask = taskRepository.findById(id);
+        if (optionalTask.isPresent()) {
+            taskRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        } else {
+            throw new ResourceNotFoundException("Task not found with id " + id);
+        }
     }
 }
