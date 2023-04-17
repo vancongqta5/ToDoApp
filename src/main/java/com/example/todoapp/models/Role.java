51c701c18c0d921cd25c57939e538c6c9d3abfef
vancongqta5 <vancongqta5@gmail.com>
2023-04-17 15:23:31 +0700

    commit new

--- src/main/java/com/example/todoapp/models/Role.java
+++ src/main/java/com/example/todoapp/models/Role.java
@@ -1,11 +1,15 @@
 package com.example.todoapp.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import javax.persistence.*;
 
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "roles")
 public class Role {
