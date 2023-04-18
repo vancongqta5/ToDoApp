1c4b30fc708e28ce2de452cd503bf322aea3697b
vancongqta5 <vancongqta5@gmail.com>
2023-04-18 12:07:46 +0700

    edit Role entiry

--- src/main/java/com/example/todoapp/models/Role.java
+++ src/main/java/com/example/todoapp/models/Role.java
@@ -5,17 +5,28 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "roles")
+@Table(name = "role")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     private String name;
+    @ManyToMany(mappedBy = "roles",cascade =
+            {
+                    CascadeType.DETACH,
+                    CascadeType.MERGE,
+                    CascadeType.REFRESH,
+                    CascadeType.PERSIST
+            }, fetch = FetchType.LAZY)
+    private List<UserEntity> users = new ArrayList<>();
+
 }
--- src/main/java/com/example/todoapp/models/UserEntity.java
+++ src/main/java/com/example/todoapp/models/UserEntity.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 @Data
 @Getter
 @Setter
