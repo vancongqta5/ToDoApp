4fdfec56a74b5cb257027a185700894a23e219f7
vancongqta5 <vancongqta5@gmail.com>
2023-04-20 11:40:39 +0700

    edit ResgisterDto: check null and spaces, size

--- src/main/java/com/example/todoapp/dto/RegisterDto.java
+++ src/main/java/com/example/todoapp/dto/RegisterDto.java
@@ -1,19 +1,20 @@
 package com.example.todoapp.dto;
 
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
 public class RegisterDto {
-    @NotNull(message = "username can't be null")
+    @NotBlank(message = "Username cannot be empty and no spaces")
     @Size(min = 6, max = 20, message = "Username must be between 6 and 20 characters")
     private String username;
     @NotNull(message = "email can't be null")
     @Email(message = "Invalid email address")
     private String email;
-    @NotNull(message = "password can't be null")
+    @NotBlank(message = "Password cannot be empty and no spaces")
+    @Size(min = 3, max = 20, message = "Password must be between 3 and 20 characters")
     private String password;
 }
