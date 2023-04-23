cdbbecef76a4faf4f2d66f85bb99df7f37388cc5
vancongqta5 <vancongqta5@gmail.com>
2023-04-23 21:24:05 +0700

    check Username and Email already exists in method register

--- src/main/java/com/example/todoapp/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/todoapp/service/impl/UserServiceImpl.java
@@ -2,17 +2,17 @@ package com.example.todoapp.service.impl;
 
 import com.example.todoapp.dto.UserRequestDto;
 import com.example.todoapp.dto.UserResponseDto;
+import com.example.todoapp.exception.userException.UserNotValidException;
 import com.example.todoapp.models.Role;
 import com.example.todoapp.models.User;
 import com.example.todoapp.repository.RoleRepository;
 import com.example.todoapp.repository.UserRepository;
 import com.example.todoapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.Collections;
-import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -21,8 +21,6 @@ public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
     @Autowired
     private RoleRepository roleRepository;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
     @Override
     public UserResponseDto register(UserRequestDto userRequestDto) {
@@ -31,6 +29,15 @@ public class UserServiceImpl implements UserService {
         user.setUsername(userRequestDto.getUsername());
         user.setEmail(userRequestDto.getEmail());
         user.setPassword(passwordEncoder.encode(userRequestDto.getPassword()));
+
+        // Kiểm tra xem username hoặc email đã tồn tại trong database chưa
+        if (userRepository.existsByUsername(user.getUsername())) {
+            throw new UserNotValidException(HttpStatus.BAD_REQUEST.value(), "Username already exists");
+        }
+
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new UserNotValidException(HttpStatus.BAD_REQUEST.value(), "Email already exists");
+        }
 
         Role role = roleRepository.findByName("USER")
                 .orElseThrow(() -> new RuntimeException("USER role not found"));
@@ -42,6 +49,7 @@ public class UserServiceImpl implements UserService {
         userResponseDto.setId(savedUser.getId());
         userResponseDto.setUsername(savedUser.getUsername());
         userResponseDto.setEmail(savedUser.getEmail());
+//        userResponseDto.setPassword(savedUser.getPassword());
         userResponseDto.setRolenames(role.getName()); // set tên của role cho userResponseDto
         return userResponseDto;
     }
